ddd82ac78160aba342d4a25c8c3b97d49b381f78
wibawaarif <arifwb.work@gmail.com>
2023-03-26 12:39:47 +0700

    initialize project with Create React App

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+import { Search } from '@mui/icons-material';
+import { IconButton, Paper } from '@mui/material';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const SearchBar = () => {
+  return (
+    <Paper component="form" onSubmit={() => {}} sx={{borderRadius: 20, border: "1px solid #e3e3e3", pl: 2, boxShadow: "none", mr: { sm: 5 } }}>
+      <input className="search-bar" placeholder="search..." value="" onChange={() => {}} />
+      <IconButton type="submit" sx={{ p: '10px', color: 'red'}}>
+        <Search />
+      </IconButton>
+    </Paper>
+  )
+}
+
+export default SearchBar
